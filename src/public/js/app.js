221222c71850985845459e37cee4c27a64ba9183
Eduardo Martin Labollita <eduardo.martin.labollita@gmail.com>
2020-10-27 21:00:29 -0300

    Events buttons

---
+++ src/public/js/app.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll("button");
+
+buttons[1].addEventListener("click", function () {
+  alert("button 1");
+});
+
+buttons.forEach(function (item) {
+  item.addEventListener("click", function () {
+    ButtonC("ejemplo de button");
+  });
+});
+
+function ButtonC(nombre) {
+  alert("ejemplo de funcion" + nombre);
+}
